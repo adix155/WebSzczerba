89184a67e4fd3855cb46a14da5be9a108ecac34f
Adrian Szczerba <adix1551@gmail.com>
2020-11-16 23:54:42 +0100

    Nauka Django cz. 1

--- WebSzczerbaApp/views.py
+++ WebSzczerbaApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import test
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<b><center>Elo, tu pierwsza strona napisana przeze mnie.</center></b>")
+    #return render(request, 'test2.html')
